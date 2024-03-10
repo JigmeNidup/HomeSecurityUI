e0bbe87acdc7a338a02d0448868dc398e311aeaa
Jigme Nidup <jigmenidup8102003@gmail.com>
2024-03-10 22:46:33 +0530

    get rfid route added

--- app/api/rfid/route.js
+++ app/api/rfid/route.js
@@ -2,6 +2,25 @@ import User from "@/models/user";
 import { connectToDB } from "@/utils/database/mongodb";
 
 //PATCH (update)
+export const GET = async (req, res) => {
+  try {
+    await connectToDB();
+    const users = await User.find();
+    let obj = {};
+    for (let i = 0; i < users.length; i++) {
+      obj[users[i].name] = users[i].rfid;
+      
+    }
+    return new Response(JSON.stringify(obj), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify("Failed to get the User RFID"), {
+      status: 500,
+    });
+  }
+};
+
 export const PATCH = async (req, res) => {
   try {
     const { id, rfid } = await req.json();
@@ -14,7 +33,7 @@ export const PATCH = async (req, res) => {
     await existingUser.save();
     return new Response(JSON.stringify(existingUser), { status: 200 });
   } catch (error) {
-    return new Response(JSON.stringify("Failed to update the User"), {
+    return new Response(JSON.stringify("Failed to update the User RFID"), {
       status: 500,
     });
   }
